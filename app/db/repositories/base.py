c18a3b2c303e9e3f1beb0113fab88d67bac0f6c1
Ilya Goldin <ilya-goldin@yandex.ru>
2022-04-10 15:25:24 +0300

    fix: add get and get_multi methods in the BaseRepository

--- app/db/repositories/base.py
+++ app/db/repositories/base.py
@@ -1,10 +1,24 @@
-from asyncpg.connection import Connection
+from typing import TypeVar, Generic, Type, Any
+
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from app.models.orm.base import Base
 
 
-class BaseRepository:
-    def __init__(self, conn: Connection) -> None:
-        self._conn = conn
+ModelType = TypeVar('ModelType', bound=Base)
+CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
+UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
 
-    @property
-    def connection(self) -> Connection:
-        return self._conn
+
+class BaseRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Type[ModelType]) -> None:
+        self.model = model
+
+    def get(self, db: Session, id_: Any) -> ModelType | None:
+        return db.query(self.model).filter(self.model.id == id_).first()
+
+    def get_multi(
+        self, db: Session, *, skip: int = 0, limit: int = 100
+    ) -> list[ModelType]:
+        return db.query(self.model).offset(skip).limit(limit).all()
