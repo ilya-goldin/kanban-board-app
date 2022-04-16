c93ede8e1e7f1a6722c21a5c6f87dd99f6715309
Ilya Goldin <ilya-goldin@yandex.ru>
2022-04-16 19:59:17 +0300

    feat: Added team repository, queries and routes

--- app/db/queries/sql/team.sql
+++ app/db/queries/sql/team.sql
@@ -1,14 +1,26 @@
 -- name: create_new_team<!
-INSERT INTO team (team_name)
+INSERT INTO teams (team_name)
 VALUES (:team_name)
 RETURNING
-    id, team_name, created_at, updated_at;
+    id, team_name;
 
 -- name: get_team_by_id^
 SELECT id,
-       team_name,
-       created_at,
-       updated_at
-FROM team
+       team_name
+FROM teams
 WHERE id = :team_id
 LIMIT 1;
+
+-- name: get_team_by_name^
+SELECT id,
+       team_name
+FROM teams
+WHERE team_name = :team_name
+LIMIT 1;
+
+-- name: get_all_teams
+SELECT id,
+       team_name
+FROM teams
+LIMIT :limit
+OFFSET :offset;
