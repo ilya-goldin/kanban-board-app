e398a49cf60c2c44a2177b7e325769a1a0de6d15
Ilya Goldin <ilya-goldin@yandex.ru>
2022-04-16 19:21:08 +0300

    fix: Remove unused methods

--- app/db/repositories/base.py
+++ app/db/repositories/base.py
@@ -1,24 +1,10 @@
-from typing import TypeVar, Generic, Type, Any
-
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
-
-from app.models.orm.base import Base
+from asyncpg.connection import Connection
 
 
-ModelType = TypeVar('ModelType', bound=Base)
-CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
-UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
+class BaseRepository:
+    def __init__(self, _connection: Connection, *args, **kwargs) -> None:
+        self._connection: Connection = _connection
 
-
-class BaseRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
-    def __init__(self, model: Type[ModelType]) -> None:
-        self.model = model
-
-    def get(self, db: Session, id_: Any) -> ModelType | None:
-        return db.query(self.model).filter(self.model.id == id_).first()
-
-    def get_multi(
-        self, db: Session, *, skip: int = 0, limit: int = 100
-    ) -> list[ModelType]:
-        return db.query(self.model).offset(skip).limit(limit).all()
+    @property
+    def connection(self) -> Connection:
+        return self._connection
