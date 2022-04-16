b3320e6b741a13771bf71e704ca446d655a11c05
Ilya Goldin <ilya-goldin@yandex.ru>
2022-04-16 19:58:05 +0300

    fix: Change logger settings

--- app/core/logging.py
+++ app/core/logging.py
@@ -6,14 +6,23 @@ from loguru import logger
 
 
 class InterceptHandler(logging.Handler):
+    loglevel_mapping = {
+        50: 'CRITICAL',
+        40: 'ERROR',
+        30: 'WARNING',
+        20: 'INFO',
+        10: 'DEBUG',
+        0: 'NOTSET',
+    }
+
     def emit(self, record: logging.LogRecord) -> None:
-        # Get corresponding Loguru level if it exists
         try:
             level = logger.level(record.levelname).name
-        except ValueError:
-            level = str(record.levelno)
+        except AttributeError:
+            level = self.loglevel_mapping[record.levelno]
+        # except ValueError:
+        #     level = str(record.levelno)
 
-        # Find caller from where originated the logged message
         frame, depth = logging.currentframe(), 2
         while frame.f_code.co_filename == logging.__file__:
             frame = cast(FrameType, frame.f_back)
